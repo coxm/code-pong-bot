239eb3be1fe54514fb40318962cc17bd8e2d9014
coxm <coxm@users.noreply.github.com>
2018-09-30 19:43:34 +0100

    Fix column name in migration

--- migrations/20180919214846_status.js
+++ migrations/20180919214846_status.js
@@ -2,6 +2,7 @@ exports.up = async knex => {
   await knex.schema.renameTable('baton', 'status');
   await knex.schema.table('status', async table => {
     table.dropColumn('time');
+    table.renameColumn('user', 'ponger');
   });
 };
 
@@ -10,5 +11,6 @@ exports.down = async knex => {
   await knex.schema.renameTable('status', 'baton');
   await knex.schema.table('baton', async table => {
     table.datetime('time').nullable();
+    table.renameColumn('ponger', 'user');
   });
 };
